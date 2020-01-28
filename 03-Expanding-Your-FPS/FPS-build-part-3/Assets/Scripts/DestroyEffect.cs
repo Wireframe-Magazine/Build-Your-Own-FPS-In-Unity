2cf1287ad9175b07db2673d981a48da9b871e1d9
ryanlambie <42672777+ryanlambie@users.noreply.github.com>
2020-01-28 14:00:53 +0000

    Expanding your first-person shooter

---
+++ 03-Expanding-Your-FPS/FPS-build-part-3/Assets/Scripts/DestroyEffect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class DestroyEffect : MonoBehaviour
+{
+    public float maxTime = 1;
+    private float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > maxTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ 03-Expanding-Your-FPS/FPS-build-part-3/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    private GameObject player;
+    private float minClamp = -45;
+    private float maxClamp = 45;
+    [HideInInspector]
+    public Vector2 rotation;
+    private Vector2 currentLookRot;
+    private Vector2 rotationV = new Vector2(0, 0);
+    public float lookSensitivity = 2;
+    public float lookSmoothDamp = 0.1f;
+
+    void Start()
+    {
+        //Access the player GameObject.
+        player = transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Player input from the mouse
+        rotation.y += Input.GetAxis("Mouse Y") * lookSensitivity;
+        //Limit ability look up and down.
+        rotation.y = Mathf.Clamp(rotation.y, minClamp, maxClamp);
+        //Rotate the character around based on the mouse X position.
+        player.transform.RotateAround(transform.position, Vector3.up, Input.GetAxis("Mouse X") * lookSensitivity);
+        //Smooth the current Y rotation for looking up and down.
+        currentLookRot.y = Mathf.SmoothDamp(currentLookRot.y, rotation.y, ref rotationV.y, lookSmoothDamp);
+        //Update the camera X rotation based on the values generated.
+        transform.localEulerAngles = new Vector3(-currentLookRot.y, 0, 0);
+    }
+}
